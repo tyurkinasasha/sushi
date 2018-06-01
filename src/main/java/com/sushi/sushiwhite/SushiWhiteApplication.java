ea456a0983be672dd2b85ebc372b460d24b22b09
Sasha <aleksandra_tiurkina@epam.com>
2018-06-01 16:59:10 +0300

    initial commit

---
+++ src/main/java/com/sushi/sushiwhite/SushiWhiteApplication.java
@@ -0,0 +1,12 @@
+package com.sushi.sushiwhite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SushiWhiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SushiWhiteApplication.class, args);
+	}
+}
